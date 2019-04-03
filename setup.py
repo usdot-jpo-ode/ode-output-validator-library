22e1acab6c5c643143690179414acbb90d35bf1b
Hamid Musavi <musavi_hamid@bah.com>
2019-04-03 11:03:42 -0400

    Some re-orgs to make the package work.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="odevalidator",
-    version="0.01",
+    version="0.0.1",
     author_email="fake@email.com",
     description="ODE Data Validation Library",
     packages=['odevalidator']
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Starting all tests..."
+echo "====="
+python odevalidator/__init__.py
+echo "====="
+echo "Testing complete."
