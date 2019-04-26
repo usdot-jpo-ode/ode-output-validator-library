c37a3ab61a3ff589a32004a38885d9438d123fae
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-04-26 12:48:08 -0400

    Break out unit tests Minor refactor of validator.py New tests for Field

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="odevalidator",
     version="0.0.3",
-    author_email="fake@email.com",
     description="ODE Data Validation Library",
     packages=find_packages(),
     package_data={'odevalidator': ['config.ini']},
---
+++ tests/validator_field_test.py
@@ -0,0 +1,56 @@
+from odevalidator import Field, ValidatorException
+import unittest
+import queue
+
+class FieldUnitTest(unittest.TestCase):
+    def test_constructor_fails_no_path(self):
+        input_field_object = {}
+        try:
+            Field(input_field_object)
+            self.fail("Expected ValidatorException")
+        except ValidatorException as e:
+            self.assertEqual("Missing required configuration property 'Path' for field '{}'", str(e))
+
+    def test_constructor_fails_no_type(self):
+        input_field_object = {"Path":"a.b.c"}
+        try:
+            Field(input_field_object)
+            self.fail("Expected ValidatorException")
+        except ValidatorException as e:
+            self.assertEqual("Missing required configuration property 'Type' for field '{'Path': 'a.b.c'}'", str(e))
+
+    def test_constructor_fails_invalid_earliesttime(self):
+        input_field_object = {"Path":"a.b.c", "Type":"timestamp", "EarliestTime":"invalidtimestamp"}
+        try:
+            Field(input_field_object)
+            self.fail("Expected ValidatorException")
+        except ValidatorException as e:
+            self.assertEqual("Unable to parse configuration file timestamp EarliestTime for field {'Path': 'a.b.c', 'Type': 'timestamp', 'EarliestTime': 'invalidtimestamp'}, error: ('Unknown string format:', 'invalidtimestamp')", str(e))
+
+    def test_constructor_fails_valid_earliesttime(self):
+        input_field_object = {"Path":"a.b.c", "Type":"timestamp", "EarliestTime":"2019-03-14T14:54:21.596Z"}
+        actual_field = Field(input_field_object)
+        self.assertEqual("2019-03-14T14:54:21.596Z", actual_field.earliest_time)
+
+    def test_constructor_fails_invalid_latesttime(self):
+        input_field_object = {"Path":"a.b.c", "Type":"timestamp", "LatestTime":"invalidtimestamp"}
+        try:
+            Field(input_field_object)
+            self.fail("Expected ValidatorException")
+        except ValidatorException as e:
+            self.assertEqual("Unable to parse configuration file timestamp LatestTime for field {'Path': 'a.b.c', 'Type': 'timestamp', 'LatestTime': 'invalidtimestamp'}, error: ('Unknown string format:', 'invalidtimestamp')", str(e))
+
+
+    # def test_get_field_value_returns_field_value_integer(self):
+    #     path_string = "alpha.beta.gamma"
+    #     data_object = {
+    #         "alpha":{
+    #             "beta":{
+    #                 "gamma":5
+    #             }
+    #         }
+    #     }
+    #
+    #     expected_value = 5
+    #     actual_value = odevalidator._
+    #     pass
